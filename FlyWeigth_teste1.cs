395ab71e2f1748447d6c84acc080a9f06f74f868
Enisch <nicholasmacielrodrigues@gmail.com>
2023-11-10 18:20:11 -0300

    Alteração básica utilizando o GitBAsh

--- FlyWeigth_teste1.cs
+++ FlyWeigth_teste1.cs
@@ -5,21 +5,21 @@ using System.Dynamic;
 
 public abstract class Tartaruga
 {
-    protected string condicao;//protected simboliza que apenas essa classe e as que a herdarem teram acesso a essa variavel;
-    protected string acao;
-    public string cor{get;set;}//nesse contexto get set servem como ler uma declaração sobre essa variavel e então defini-lô como parte da mesma.
+    protected string? condicao;//protected simboliza que apenas essa classe e as que a herdarem terão acesso a essa variavel;
+    protected string? acao;
+    public string? cor{get;set;}
 
     public abstract void Mostra(string cor);//Metodo que servira posteriormente(Assim que for sobrescrito) para mostrar as caracteristicas das tartarugas na tela.
 }
 
 public class Vermelha:Tartaruga
 {
-    public Vermelha()//Construtor que iniciara as variaveis.
+    public Vermelha()//Construtor 
     {
-        this.condicao=" tartaruga dentro do casco.";//o This foi usado e deve ser usado sempre que uma variavel de outra classe tiver seu valor "alterado".
+        this.condicao=" tartaruga dentro do casco.";
         this.acao=" Rodando no chão.\nCor:";
     }
-    public override void Mostra(string qualcor)//O parametro qualcor é uma variavel, criada neste instante, que posteriormente poderá ser declara, no Main, como cor.Vide o exemplo a seguir: tartaruga.Mostra(cor);
+    public override void Mostra(string qualcor)//;
     {
         this.cor=qualcor;
         Console.WriteLine(condicao+ acao+ cor.ToUpper());
@@ -62,12 +62,12 @@ public class Azul:Tartaruga
 
 public class FlyWheight
 {
-    private Dictionary<string, Tartaruga> lista_de_tartarugas=new Dictionary<string, Tartaruga>();//Semelhante a criação de classes e listas, porém, devemos declarar o modificador de acesso antes.
+    private Dictionary<string, Tartaruga> lista_de_tartarugas=new Dictionary<string, Tartaruga>();//Semelhante a criação de classes e listas, porém, devemos declarar o modificador de acesso antes.No caso TKey;
 
     public Tartaruga GetTartaruga(string cor)//A classe tartaruga foi evoca e então criamos um metodo GetTartaruga com o parametro cor. Acredito se tratar de uma variavel diferente da outra já declarada.
     {
-        Tartaruga t=null;// criamos uma variavel na classe tartaruga cujo valor é nulo.
-        if(lista_de_tartarugas.ContainsKey(cor))//Dictionary<Tkey,Tvalue>.ContainsKey(string)=> Declaramos um dicionario cujo o Tkey é uma string e o ContainsKey referencia que a key na verdade é cor.Se o dicionario possuir cor ele é verdade e portanto t=Dictionary;
+        Tartaruga? t=null;// criamos uma variavel na classe tartaruga cujo valor é nulo.
+        if(lista_de_tartarugas.ContainsKey(cor))
         {
             t= lista_de_tartarugas[cor];
         }
@@ -79,9 +79,9 @@ public class FlyWheight
                 case "Vermelha": t=new Vermelha(); break;
                 case "Amarela": t=new Amarela(); break;
             }
-            lista_de_tartarugas.Add(cor,t);
+            lista_de_tartarugas.Add(cor,t!);
         }
-        return t;
+        return t!;
     }
 }
 
@@ -90,7 +90,7 @@ class A1
     static void Main(string[] args)
     {
         FlyWheight flyWheight=new FlyWheight();
-        string cor=string.Empty;
+        string? cor;
 
         Tartaruga tartaruga;
 
@@ -100,8 +100,8 @@ class A1
             Console.WriteLine("Qual tartaruga enviar para a tela: ");
             cor = Console.ReadLine();
 
-            tartaruga = flyWheight.GetTartaruga(cor);//tartaruga ocupa o mesmo espaço na memoria que flyWeight.GetTartaruga(cor) e portanto são iguais.
-            tartaruga.Mostra(cor);
+            tartaruga = flyWheight.GetTartaruga(cor!);//tartaruga ocupa o mesmo espaço na memoria que flyWeight.GetTartaruga(cor) e portanto são iguais.
+            tartaruga.Mostra(cor!);
 
             Console.WriteLine();
             Console.WriteLine("--------------");
@@ -111,3 +111,4 @@ class A1
     }
 }
 
+//Alterações em algumas variaveis tornando-as anúlaveis e remoção de comentarios errados.
